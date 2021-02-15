a23acf8444f4bb743ee6f4affafd8f4e7a2c4116
Jonathan <jtrembla@cstj.qc.ca>
2021-02-15 10:45:16 -0500

    Ajout des fichiers du TP1

---
+++ Assets/Scripts/Perso.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe qui controle les deplacements du personnage
+/// Auteurs du code: Prenom1 Nom1 et Prenom2 Nom2
+/// Auteur des commentaires: Prenom Nom (chacun doit commenter sa version)
+/// </summary>
+public class Perso : MonoBehaviour
+{
+    
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+
+    }
+
+}
